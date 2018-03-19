337d6e2d6d3744810a4effcd8f778b3ee8f399e0
haozheng <hza89@sfu.ca>
2018-03-18 18:34:17 -0700

    update GridView

---
+++ app/src/main/java/com/example/android/mygarden/GridWidgetService.java
@@ -0,0 +1,108 @@
+package com.example.android.mygarden;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.view.View;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.example.android.mygarden.provider.PlantContract;
+import com.example.android.mygarden.utils.PlantUtils;
+
+import static com.example.android.mygarden.provider.PlantContract.BASE_CONTENT_URI;
+import static com.example.android.mygarden.provider.PlantContract.PATH_PLANTS;
+
+/**
+ * Created by zhenghao on 2018-03-18.
+ */
+
+public class GridWidgetService extends RemoteViewsService{
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new GridRemoteViewFactory(this.getApplicationContext());
+    }
+}
+
+class GridRemoteViewFactory implements RemoteViewsService.RemoteViewsFactory {
+    Context mContext;
+    Cursor mCursor;
+
+    public GridRemoteViewFactory(Context applicationContext) {
+        mContext = applicationContext;
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    // Called on start and when notifyAppWidgetViewDataChanged is called
+    @Override
+    public void onDataSetChanged() {
+        // Get all plant info ordered by creation time
+        Uri PLANT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build();
+        if (mCursor != null) mCursor.close();
+        mCursor = mContext.getContentResolver().query(
+                PLANT_URI,
+                null,
+                null,
+                null,
+                PlantContract.PlantEntry.COLUMN_CREATION_TIME
+        );
+    }
+
+    @Override
+    public void onDestroy() {
+        mCursor.close();
+    }
+
+    @Override
+    public int getCount() {
+        if (mCursor == null) return 0;
+        return mCursor.getCount();
+    }
+
+    /**
+     +     * This method acts like the onBindViewHolder method in an Adapter
+     +     *
+     +     * @param position The current position of the item in the GridView to be displayed
+     +     * @return The RemoteViews object to display for the provided postion
+     +     */
+    @Override
+    public RemoteViews getViewAt(int position) {
+        if (mCursor == null || mCursor.getCount() == 0) {
+            return null;
+            mCursor.moveToPosition(position);
+            int idIndex = mCursor.getColumnIndex(PlantContract.PlantEntry._ID);
+            int createTimeIndex = mCursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_CREATION_TIME);
+            int waterTimeIndex = mCursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME);
+            int plantTyoeIndex = mCursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_PLANT_TYPE);
+
+            long plantId = mCursor.getLong(idIndex);
+            int plantType = mCursor.getInt(plantTyoeIndex);
+            long createdAt = mCursor.getLong(createTimeIndex);
+            long wateredAt = mCursor.getLong(waterTimeIndex);
+            long timeNow = System.currentTimeMillis();
+
+            RemoteViews views = new RemoteViews(mContext.getPackageName(), R.layout.plant_widget);
+
+            // Update the plant image
+            int imgRes = PlantUtils.getPlantImageRes(mContext, timeNow-createdAt, timeNow-wateredAt, plantType);
+            views.setImageViewResource(R.id.widget_plant_image, imgRes);
+            views.setTextViewText(R.id.widget_plant_name, String.valueOf(plantId));
+            // Always hide the water drop in GridView mode
+            views.setViewVisibility(R.id.widget_water_button, View.GONE);
+
+
+
+        }
+    }
+
+
+
+
+
+
+}
