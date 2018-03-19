47708a7d7a73a54e7c3cd48e7669897d5ef0fa4d
haozheng <hza89@sfu.ca>
2018-03-18 22:49:40 -0700

    update SinglePlant

--- app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -16,11 +16,14 @@ package com.example.android.mygarden;
 * limitations under the License.
 */
 
+import android.annotation.TargetApi;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.RemoteViews;
@@ -31,6 +34,8 @@ import com.example.android.mygarden.ui.PlantDetailActivity;
 
 public class PlantWidgetProvider extends AppWidgetProvider {
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+
     // setImageViewResource to update the widget’s image
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int imgRes, long plantId, boolean showWater, int appWidgetId) {
@@ -38,35 +43,48 @@ public class PlantWidgetProvider extends AppWidgetProvider {
         // TODO (4): separate the updateAppWidget logic into getGardenGridRemoteView and getSinglePlantRemoteView
         // TODO (5): Use getAppWidgetOptions to get widget width and use the appropriate RemoteView method
         // TODO (6): Set the PendingIntent template in getGardenGridRemoteView to launch PlantDetailActivity
-        
-        Intent intent;
-        if (plantId == PlantContract.INVALID_PLANT_ID) {
-            intent = new Intent(context, MainActivity.class);
-        } else { // Set on click to open the corresponding detail activity
-            Log.d(PlantWidgetProvider.class.getSimpleName(), "plantId=" + plantId);
-            intent = new Intent(context, PlantDetailActivity.class);
-            intent.putExtra(PlantDetailActivity.EXTRA_PLANT_ID, plantId);
+
+        // Get current width to decide on single plant vs garden grid view
+        Bundle options = appWidgetManager.getAppWidgetOptions(appWidgetId);
+        int width = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+        RemoteViews rv;
+        if (width < 300) {
+            rv = getSinglePlantRemoteView(context, imgRes, plantId, showWater);
+        } else {
+            rv = getGardenGridRemoteView(context);
         }
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
-        // Update image
-        views.setImageViewResource(R.id.widget_plant_image, imgRes);
-        // Update plant ID text
-        views.setTextViewText(R.id.widget_plant_name, String.valueOf(plantId));
-        // Show/Hide the water drop button
-        if (showWater) views.setViewVisibility(R.id.widget_water_button, View.VISIBLE);
-        else views.setViewVisibility(R.id.widget_water_button, View.INVISIBLE);
-        // Widgets allow click handlers to only launch pending intents
-        views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
-        // Add the wateringservice click handler
-        Intent wateringIntent = new Intent(context, PlantWateringService.class);
-        wateringIntent.setAction(PlantWateringService.ACTION_WATER_PLANT);
-        wateringIntent.putExtra(PlantWateringService.EXTRA_PLANT_ID, plantId);
-        PendingIntent wateringPendingIntent = PendingIntent.getService(context, 0, wateringIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        views.setOnClickPendingIntent(R.id.widget_water_button, wateringPendingIntent);
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        appWidgetManager.updateAppWidget(appWidgetId, rv);
+
+
+//        Intent intent;
+//        if (plantId == PlantContract.INVALID_PLANT_ID) {
+//            intent = new Intent(context, MainActivity.class);
+//        } else { // Set on click to open the corresponding detail activity
+//            Log.d(PlantWidgetProvider.class.getSimpleName(), "plantId=" + plantId);
+//            intent = new Intent(context, PlantDetailActivity.class);
+//            intent.putExtra(PlantDetailActivity.EXTRA_PLANT_ID, plantId);
+//        }
+//        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+//        // Construct the RemoteViews object
+//        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
+//        // Update image
+//        views.setImageViewResource(R.id.widget_plant_image, imgRes);
+//        // Update plant ID text
+//        views.setTextViewText(R.id.widget_plant_name, String.valueOf(plantId));
+//        // Show/Hide the water drop button
+//        if (showWater) views.setViewVisibility(R.id.widget_water_button, View.VISIBLE);
+//        else views.setViewVisibility(R.id.widget_water_button, View.INVISIBLE);
+//        // Widgets allow click handlers to only launch pending intents
+//        views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
+
+//        // Add the wateringservice click handler
+//        Intent wateringIntent = new Intent(context, PlantWateringService.class);
+//        wateringIntent.setAction(PlantWateringService.ACTION_WATER_PLANT);
+//        wateringIntent.putExtra(PlantWateringService.EXTRA_PLANT_ID, plantId);
+//        PendingIntent wateringPendingIntent = PendingIntent.getService(context, 0, wateringIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+//        views.setOnClickPendingIntent(R.id.widget_water_button, wateringPendingIntent);
+//        // Instruct the widget manager to update the widget
+//        appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     @Override
@@ -81,6 +99,51 @@ public class PlantWidgetProvider extends AppWidgetProvider {
             updateAppWidget(context, appWidgetManager, imgRes, plantId, showWater, appWidgetId);
         }
     }
+
+    // Create and returns the RemoteViews to be displayed in the single plant mode widget
+    private static RemoteViews getSinglePlantRemoteView(Context context, int imgRes, long plantId,
+                                                        boolean showWater) {
+        // Set the click handler to open the DetailActivity for plant ID
+        // or the MainActivity if plant ID is invalid
+        Intent intent;
+        if (plantId == PlantContract.INVALID_PLANT_ID) {
+            intent = new Intent(context, MainActivity.class);
+        } else {
+            // Set on click to open the corresponding detail activity
+            Log.d(PlantWidgetProvider.class.getSimpleName(), "planId=" + plantId);
+            intent = new Intent(context, PlantDetailActivity.class);
+            intent.putExtra(PlantDetailActivity.EXTRA_PLANT_ID, plantId);
+        }
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
+        // Update image and text
+        views.setImageViewResource(R.id.widget_plant_image, imgRes);
+        views.setTextViewText(R.id.widget_plant_name, String.valueOf(plantId));
+        // Show/Hide the water drop button
+        if (showWater) {
+            views.setViewVisibility(R.id.widget_water_button, View.VISIBLE);
+        } else {
+            views.setViewVisibility(R.id.widget_water_button, View.INVISIBLE);
+
+        }
+        // Widgets allow click handlers to only launch pending intents
+        views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
+        // Add the wateringservice click handler
+        Intent wateringIntent = new Intent(context, PlantWateringService.class);
+        wateringIntent.setAction(PlantWateringService.ACTION_WATER_PLANT);
+        // Add the plant ID as extra to water only that plant when clicked
+        wateringIntent.putExtra(PlantWateringService.EXTRA_PLANT_ID, plantId);
+        PendingIntent wateringPendingIntent = PendingIntent.getService(context, 0,
+                wateringIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        views.setOnClickPendingIntent(R.id.widget_water_button, wateringPendingIntent);
+        return views;
+
+    }
+
+
 
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
